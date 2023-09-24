f42f78aa34952d7ad7e1a03871d14f872ba91b81
Ashkeyev Ali <ashkeyevli@gmail.com>
2023-09-24 21:08:27 +0600

    HW2

---
+++ HW2/to-do/src/MyList.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React, {Component} from "react";
+
+class MyList extends Component {
+    render() {
+        return (
+            <div>
+                <h1> Things to-do:</h1>
+
+                <ul>
+                    <li>ssdsd</li>
+                    <li>ssdsd</li>
+                    <li>ssdsd</li>
+                    <li>ssdsd</li>
+                </ul>
+            </div>
+
+        )
+    }
+}
+export default MyList
